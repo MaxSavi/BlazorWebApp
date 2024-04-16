0ed7049b7ce2e6f011f07cc6f70b0697e96f6827
MaxSavi <137710036+MaxSavi@users.noreply.github.com>
2024-04-16 11:59:45 +0300

    Добавьте файлы проекта.

---
+++ Data/BlazorWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebApp.Models;
+
+namespace BlazorWebApp.Data
+{
+    public class BlazorWebAppContext : DbContext
+    {
+        public BlazorWebAppContext (DbContextOptions<BlazorWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebApp.Models.BookModel> BookModel { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using BlazorWebApp.Components;
+using BlazorWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BlazorWebAppContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("BlazorWebAppContext") ?? throw new InvalidOperationException("Connection string 'BlazorWebAppContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+//public void ConfigureServices(IServiceCollection services)
+//{
+//    services.AddDbContext<BookContext>(options =>
+//        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+//    // ������ �������
+//}
+//builder.Services.AddDbContext<BookContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("BookContext") ?? throw new InvalidOperationException("Connection string 'BookContext' not found.")));
+
+
+app.Run();
